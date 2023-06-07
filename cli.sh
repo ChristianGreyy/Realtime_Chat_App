4c8cbe52af0bd38005acc786a889d05003117b50
TruongThanhHung <hung.tt@adamo.software>
2023-06-07 10:00:19 +0700

    :package: Set up packages

---
+++ cli.sh
@@ -0,0 +1,30 @@
+# Sequelize
+npm install --save @nestjs/sequelize sequelize sequelize-typescript pg-hstore pg
+npm install --save-dev @types/sequelize
+
+# Dotenv
+npm i @nestjs/config
+npm install dotenv --save
+
+# Passport
+npm install --save @nestjs/passport passport passport-local
+npm install --save-dev @types/passport-local
+
+npm install --save @nestjs/jwt passport-jwt
+npm install --save-dev @types/passport-jwt
+
+# Bcrypt
+npm install bcrypt --save
+
+# Jwt
+npm install --save @nestjs/jwt jsonwebtoken
+
+# Socket
+npm i --save @nestjs/websockets @nestjs/platform-socket.io
+
+
+
+
+
+
+
