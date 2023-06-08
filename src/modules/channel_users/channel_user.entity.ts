df23ad7dab3262cd76fa6060a7cea170bb0bafbd
TruongThanhHung <hung.tt@adamo.software>
2023-06-08 08:19:47 +0700

    :wrench: Set up channel_users

---
+++ src/modules/channel_users/channel_user.entity.ts
@@ -0,0 +1,31 @@
+import {
+  Table,
+  Column,
+  Model,
+  CreatedAt,
+  UpdatedAt,
+  ForeignKey,
+} from 'sequelize-typescript';
+import { User } from '../users/user.entity';
+import { Channel } from '../channels/channel.entity';
+
+@Table({
+  tableName: 'ChannelUsers',
+})
+export class ChannelUser extends Model<ChannelUser> {
+  @ForeignKey(() => User)
+  @Column({ field: 'user_id' })
+  userId: number;
+
+  @ForeignKey(() => Channel)
+  @Column({ field: 'channel_id' })
+  channelId: number;
+
+  @CreatedAt
+  @Column({ field: 'createdAt' })
+  createdAt: Date;
+
+  @UpdatedAt
+  @Column({ field: 'updatedAt' })
+  updatedAt: Date;
+}
